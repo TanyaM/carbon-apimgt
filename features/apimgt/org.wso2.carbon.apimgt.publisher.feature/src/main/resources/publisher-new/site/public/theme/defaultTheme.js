d548d0afa606dd7a95fe2311bd35881774318a5e
TanyaM <tanyamadurapperuma@gmail.com>
2019-08-21 23:05:03 +0530

    Fix issues in default theme

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher-new/site/public/theme/defaultTheme.js
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher-new/site/public/theme/defaultTheme.js
@@ -1,4 +1,3 @@
-const APP_CONTEXT = '/publisher-new';
 const Configurations = {
     themes: {
         light: {
@@ -11,12 +10,12 @@ const Configurations = {
                 },
                 secondary: {
                     light: '#0066ff',
-                    main: '#a2ecf5',
+                    main: '#99c573',
                     // dark: will be calculated from palette.secondary.main,
                     contrastText: '#ffcc00',
                 },
                 background: {
-                    default: '#f6f6f6',
+                    default: '#efefef',
                     paper: '#ffffff',
                     appBar: '#1d344f',
                     leftMenu: '#1a1f2f',
@@ -28,12 +27,6 @@ const Configurations = {
             typography: {
                 fontFamily: '"Open Sans", "Helvetica", "Arial", sans-serif',
                 fontSize: 12,
-                subtitle2: {
-                    fontWeight: 600,
-                },
-                h4: {
-                    fontSize: '1.3rem',
-                },
             },
             custom: {
                 starColor: '#f2c73a',
@@ -41,15 +34,18 @@ const Configurations = {
                 leftMenuWidth: 200,
                 contentAreaWidth: 1240,
                 drawerWidth: 200,
-                logo: APP_CONTEXT + '/site/public/images/logo.png',
-                backgroundImage: APP_CONTEXT + '/site/public/images/back-light.png',
-                title: 'WSO2 APIM Publisher',
+                logo: '/store-new/site/public/images/logo.png',
+                backgroundImage: '/store-new/site/public/images/back-light.png',
+                title: 'WSO2 Developer Portal',
                 defaultApiView: 'grid', // Sets the default view for the api listing page ( Other values available = 'list' )
                 showApiHelp: false, // API detials page has a right hand side panel showing it's related help. Set this to false if you want to hide it.
                 leftMenu: 'icon left', //  other values ('icon top', 'icon left', 'no icon', 'no text')
                 leftMenuIconSize: 24,
                 leftMenuIconMainSize: 52,
                 leftMenuTextStyle: 'uppercase',
+                adminRole: 'admin',
+                commentsLimit: 5,
+                maxCommentLength: 1300,
                 resourceChipColors: {
                     get: '#02a8f4',
                     post: '#8ac149',
@@ -65,6 +61,20 @@ const Configurations = {
                     subscription: '#ffcc80',
                 },
                 thumbnail: {
+                    width: 240,
+                    contentPictureOverlap: false,
+                    contentBackgroundColor: 'rgba(239, 239, 239, 0.5)',
+                    defaultApiImage: false, // put false to render the system generated and user provided image.
+                    // And put a string to render a custom image
+                    backgrounds: [
+                        // These backgrounds are use to generate the thumbnails.
+                        { prime: 0x8f6bcaff, sub: 0x4fc2f8ff },
+                        { prime: 0xf47f16ff, sub: 0xcddc39ff },
+                        { prime: 0xf44236ff, sub: 0xfec107ff },
+                        { prime: 0x2196f3ff, sub: 0xaeea00ff },
+                        { prime: 0xff9700ff, sub: 0xffeb3cff },
+                        { prime: 0xff9700ff, sub: 0xfe5722ff },
+                    ],
                     document: {
                         icon: 'library_books',
                         backgrounds: {
@@ -72,106 +82,70 @@ const Configurations = {
                             sub: 0xe2fff7ff,
                         },
                     },
-                    backgrounds: [ // These backgrounds are use to generate the thumbnails.
-                        { prime: 0x8f6bcaff, sub: 0x4fc2f8ff },
-                        { prime: 0xf47f16ff, sub: 0xcddc39ff },
-                        { prime: 0xf44236ff, sub: 0xfec107ff },
-                        { prime: 0x2196f3ff, sub: 0xaeea00ff },
-                        { prime: 0xff9700ff, sub: 0xffeb3cff },
-                        { prime: 0xff9700ff, sub: 0xfe5722ff },
-                    ],
                 },
-                // lifeCycleImage: APP_CONTEXT + '/public/app/images/logo.png',
-                // Uncomment above if you want to add a custom image to the lifecycle diagram.
-                adminRole: 'admin',
-                commentsLimit: 5,
-                maxCommentLength: 1300,
+                landingPage: {
+                    active: true,
+                    carousel: {
+                        active: true,
+                        slides: [
+                            {
+                                src: '/store-new/site/public/images/landing/01.jpg',
+                                title: 'Lorem <span>ipsum</span> dolor sit amet',
+                                content:
+                                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer felis lacus, placerat vel condimentum in, porta a urna. Suspendisse dolor diam, vestibulum at molestie dapibus, semper eget ex. Morbi sit amet euismod tortor.',
+                            },
+                            {
+                                src: '/store-new/site/public/images/landing/02.jpg',
+                                title: 'Curabitur <span>malesuada</span> arcu sapien',
+                                content:
+                                    'Curabitur malesuada arcu sapien, suscipit egestas purus efficitur vitae. Etiam vulputate hendrerit venenatis. ',
+                            },
+                            {
+                                src: '/store-new/site/public/images/landing/03.jpg',
+                                title: 'Nam vel ex <span>feugiat</span> nunc laoreet',
+                                content:
+                                    'Nam vel ex feugiat nunc laoreet elementum. Duis sed nibh condimentum, posuere risus a, mollis diam. Vivamus ultricies, augue id pulvinar semper, mauris lorem bibendum urna, eget tincidunt quam ex ut diam.',
+                            },
+                        ],
+                    },
+                    listByTag: {
+                        active: true,
+                        content: [
+                            {
+                                tag: 'finance',
+                                title: 'Checkout our Finance APIs',
+                                description:
+                                    'We offers online payment solutions and has more than 123 million customers worldwide. The WSO2 Finane API makes powerful functionality available to developers by exposing various features of our platform. Functionality includes but is not limited to invoice management, transaction processing and account management.',
+                                maxCount: 5,
+                            },
+                            {
+                                tag: 'weather',
+                                title: 'Checkout our Weather APIs',
+                                description:
+                                    'We offers online payment solutions and has more than 123 million customers worldwide. The WSO2 Finane API makes powerful functionality available to developers by exposing various features of our platform. Functionality includes but is not limited to invoice management, transaction processing and account management.',
+                                maxCount: 5,
+                            },
+                        ],
+                    },
+                    parallax: {
+                        active: true,
+                        content: [
+                            {
+                                src: '/store-new/site/public/images/landing/parallax1.jpg',
+                                title: 'Lorem <span>ipsum</span> dolor sit amet',
+                                content:
+                                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer felis lacus, placerat vel condimentum in, porta a urna. Suspendisse dolor diam, vestibulum at molestie dapibus, semper eget ex. Morbi sit amet euismod tortor.',
+                            },
+                            {
+                                src: '/store-new/site/public/images/landing/parallax2.jpg',
+                                title: 'Nam vel ex <span>feugiat</span> nunc laoreet',
+                                content:
+                                    'Nam vel ex feugiat nunc laoreet elementum. Duis sed nibh condimentum, posuere risus a, mollis diam. Vivamus ultricies, augue id pulvinar semper, mauris lorem bibendum urna, eget tincidunt quam ex ut diam.',
+                            },
+                        ],
+                    },
+                },
             },
         },
-        dark: {
-            palette: {
-                primary: {
-                    // light: will be calculated from palette.primary.main,
-                    main: '#15b8cf',
-                    // dark: will be calculated from palette.primary.main,
-                    // contrastText: will be calculated to contrast with palette.primary.main
-                },
-                secondary: {
-                    light: '#0066ff',
-                    main: '#a2ecf5',
-                    // dark: will be calculated from palette.secondary.main,
-                    contrastText: '#ffcc00',
-                },
-                background: {
-                    default: '#f6f6f6',
-                    paper: '#ffffff',
-                    appBar: '#1d344f',
-                    leftMenu: '#1a1f2f',
-                    leftMenuActive: '#000000',
-                    drawer: '#1a1f2f',
-                },
-            },
-            typography: {
-                fontFamily: '"Open Sans", "Helvetica", "Arial", sans-serif',
-                fontSize: 12,
-                subtitle2: {
-                    fontWeight: 600,
-                },
-                h4: {
-                    fontSize: '1.3rem',
-                },
-            },
-            custom: {
-                starColor: '#f2c73a',
-                disableColor: '#D3D3D3',
-                leftMenuWidth: 200,
-                contentAreaWidth: 1240,
-                drawerWidth: 200,
-                logo: APP_CONTEXT + '/site/public/images/logo.png',
-                backgroundImage: APP_CONTEXT + '/site/public/images/back-light.png',
-                title: 'WSO2 APIM Publisher',
-                defaultApiView: 'grid', // Sets the default view for the api listing page ( Other values available = 'list' )
-                showApiHelp: false, // API detials page has a right hand side panel showing it's related help. Set this to false if you want to hide it.
-                leftMenu: 'icon left', //  other values ('icon top', 'icon left', 'no icon', 'no text')
-                leftMenuIconSize: 24,
-                leftMenuIconMainSize: 52,
-                leftMenuTextStyle: 'uppercase',
-                resourceChipColors: {
-                    get: '#02a8f4',
-                    post: '#8ac149',
-                    put: '#ff9700',
-                    delete: '#fd5621',
-                    option: '#5f7c8a',
-                    patch: '#785446',
-                    head: '#785446',
-                },
-                operationChipColor: {
-                    query: '#b3e6fe',
-                    mutation: '#c1dea0',
-                    subscription: '#ffcc80',
-                },
-                thumbnail: {
-                    backgrounds: [ // These backgrounds are use to generate the thumbnails.
-                        { prime: 0x8f6bcaff, sub: 0x4fc2f8ff },
-                        { prime: 0xf47f16ff, sub: 0xcddc39ff },
-                        { prime: 0xf44236ff, sub: 0xfec107ff },
-                        { prime: 0x2196f3ff, sub: 0xaeea00ff },
-                        { prime: 0xff9700ff, sub: 0xffeb3cff },
-                        { prime: 0xff9700ff, sub: 0xfe5722ff },
-                    ],
-                },
-                // lifeCycleImage: APP_CONTEXT + '/public/app/images/logo.png',
-                // Uncomment above if you want to add a custom image to the lifecycle diagram.
-                adminRole: 'admin',
-                commentsLimit: 5,
-                maxCommentLength: 1300,
-            },
-        },
-    },
-    app: {
-        context: APP_CONTEXT,
     },
 };
-if (typeof module !== 'undefined') {
-    module.exports = Configurations; // Added for tests
-}
